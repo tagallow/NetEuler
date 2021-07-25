35dc70b3ab32511bebd78eba5ef61eac965d9882
Tom Galloway <tagallow@outlook.com>
2021-07-25 06:45:47 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
